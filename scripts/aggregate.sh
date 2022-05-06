792ed2b1874d95c07bd718a8bf152e435b69a379
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-05-06 20:47:53 +0900

    aggregate.sh to copy to clipboard

--- scripts/aggregate.sh
+++ scripts/aggregate.sh
@@ -8,3 +8,16 @@ find steps/* -print0 | \
   grep -v '#!/bin/sh' | \
   grep -v '# REMOVE THIS' | \
   sed 's/^\#\s//' > README.md
+
+# copy README.md to the clipboard
+if which clip.exe 1>/dev/null # WSL
+then
+  clip.exe < README.md
+elif which pbcopy  1>/dev/null # Mac
+then
+  pbcopy < README.md
+elif which xsel  1>/dev/null # Debian/Ubuntu Linux
+then
+  # WSL gives error on xsel as "xsel: Can't open display: (null)" so, using clip.exe on WSL
+  xsel --clipboard --input < README.md
+fi
