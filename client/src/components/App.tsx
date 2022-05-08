e0e11afa639ed8e508398589baf8fde2821bbf00
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-05-08 09:53:23 +0900

    Add ApolloClient to App.tsx

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,10 +1,16 @@
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import { HeaderContainer } from "./header/HeaderContainer";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <HeaderContainer />
-    </>
+    </ApolloProvider>
   );
 }
 
