c894200cf8a1995e2155d941c2fff58bacdfd4da
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-05-08 14:53:03 +0900

    decorate DivisionCard title

--- client/src/components/company/DivisionCard.tsx
+++ client/src/components/company/DivisionCard.tsx
@@ -9,12 +9,38 @@ export const DivisionCard = ({ fragment }: DivisionCardProps): JSX.Element => {
   return (
     <div
       style={{
-        backgroundColor: "#ffffff",
-        height: "180px",
         filter: "drop-shadow(6px 6px 6px #00000029)",
       }}
     >
-      {fragment.divisionDisplayName}
+      <div
+        style={{
+          height: "50px",
+          borderRadius: "8px 8px 0px 0px",
+          backgroundColor: "#1470C3",
+          padding: "2px 8px",
+        }}
+      >
+        <span
+          style={{
+            color: "#f3f3f3",
+            fontFamily: "'Noto Sans JP', sans-serif",
+            fontSize: "28px",
+            fontWeight: "700",
+            lineHeight:
+              "50px" /* same as height of enclosing <div>: vertical centering */,
+          }}
+        >
+          {fragment.divisionDisplayName}
+        </span>
+      </div>
+      <div
+        //background
+        style={{
+          height: "130px",
+          borderRadius: "0px 0px 8px 8px",
+          backgroundColor: "#ffffff",
+        }}
+      ></div>
     </div>
   );
 };
