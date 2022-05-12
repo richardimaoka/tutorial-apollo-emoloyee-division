bd306ac73e4c58bec75b355c2fd4654437a302b3
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-05-12 23:57:48 +0900

    update steps up to 5

--- scripts/gitlog-patch.sh
+++ scripts/gitlog-patch.sh
@@ -1,4 +1,3 @@
-	//step			
 git show --patch 201ea30 > patches/201ea30.patch
 git show --patch f482a1d > patches/f482a1d.patch
 git show --patch b979ec5 > patches/b979ec5.patch
@@ -6,7 +5,6 @@ git show --patch 425a387 > patches/425a387.patch
 git show --patch 5847c01 > patches/5847c01.patch
 git show --patch ac56d80 > patches/ac56d80.patch
 		 	                  
-	//step	 	                 
 git show --patch 88b1ced > patches/88b1ced.patch
 git show --patch 1f78fa0 > patches/1f78fa0.patch
 git show --patch af44310 > patches/af44310.patch
@@ -14,7 +12,6 @@ git show --patch 1de3257 > patches/1de3257.patch
 git show --patch 8cb5512 > patches/8cb5512.patch
 		 	                
 		 	               
-	//step	 	              
 git show --binary --patch d569883 > patches/d569883.patch
 git show --patch 6186b50 > patches/6186b50.patch
 git show --patch bca253f > patches/bca253f.patch
@@ -24,7 +21,6 @@ git show --patch c5201f0 > patches/c5201f0.patch
 git show --patch f2f7d08 > patches/f2f7d08.patch
 git show --patch a1d6ca9 > patches/a1d6ca9.patch
 		 	             
-	//step	 	            
 git show --patch 6dae1d9 > patches/6dae1d9.patch
 git show --patch 0cdd81d > patches/0cdd81d.patch
 git show --patch 56bb5dc > patches/56bb5dc.patch
@@ -32,7 +28,6 @@ git show --patch db72e9f > patches/db72e9f.patch
 git show --patch f28d1c1 > patches/f28d1c1.patch
 git show --patch 29c4cb7 > patches/29c4cb7.patch
 		 	           
-	//setp	 	          
 git show --patch 6e1739f > patches/6e1739f.patch
 git show --patch fc58873 > patches/fc58873.patch
 git show --patch fc67eea > patches/fc67eea.patch
--- steps/5-incremental-dev copy.sh
+++ steps/5-incremental-dev copy.sh
@@ -5,101 +5,82 @@ cd ../ || exit               # REMOVE THIS IN aggregate.sh - cd to the git repos
 
 ## 5. インクリメンタルな開発
 
-# さあ、いよいよ…
+# さあ、いよいよここからはReactクライアント画面の表示を次々に変化させていく、インクリメンタルな開発を行います。ソースコードの変更がすぐに画面の表示の変化となって表れるので、きっと手応えを感じていただけると思います。
 
-# :large_orange_diamond: Action: 新しいターミナルを立ち上げてください。
+# 以前に立ち上げた、4つのプロセスとは別のターミナルを引き続き使います。
 
-# ![アートボード 6.png](https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/75738/83e08d04-4c90-31f8-e4fd-62accf7f6da4.png)
+# ![アートボード 5.png](https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/75738/928b1a5f-84c8-e803-74b6-7386542a4f56.png)
 
 # :large_orange_diamond: Action: 以下のコマンドを入力してください。
+
+# まずはGraphQLサーバーから取得した部署の名前を表示しましょう。
 
 # ```terminal
 # # gitレポジトリのルートディレクトリに移動
 # shellcheck disable=SC2164 # REMOVE THIS IN aggregate.sh
 cd "$(git rev-parse --show-toplevel)"
-git apply
-bd67a1f update DivisionCard.tsx to take generated DivisionCardFragment
-# ```
-
-# :large_orange_diamond: Action: 以下のコマンドを入力してください。
-
-# ```terminal
-git apply
-b7bd9c9 CompanyPage.tsx
+git apply patches/fc58873.patch
+git apply patches/fc67eea.patch
 # ```
 
 # :white_check_mark: Result: 以下のように表示されればOKです
 
-# ![]()
+# ![image.png](https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/75738/fa3b30cc-253c-3685-6187-a7c4275de021.png)
+
+# 次に、部署名の表示を日本語に変えます。
 
 # :large_orange_diamond: Action: 以下のコマンドを入力してください。
 
 # ```terminal
-git apply
-4bdffb8 Add divisionDisplayName
+git apply patches/8af8eb9.patch
 # ```
 
 # :white_check_mark: Result: 以下のように表示されればOKです
 
-# ![]()
+# ![image.png](https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/75738/8f834abc-737e-4a34-15b0-bdbe1f91d34f.png)
 
 # DivisionCardの見た目を整えていきましょう。
 
 # :large_orange_diamond: Action: 以下のコマンドを入力してください。
 
 # ```terminal
-git apply
-309b3b6 make DivisionCard as cards visually
+git apply patches/c40553d.patch
 # ```
 
 # :white_check_mark: Result: 以下のように表示されればOKです
 
-# ![]()
-
-# :large_orange_diamond: Action: 以下のコマンドを入力してください。
-
-# ```terminal
-git apply
-0214b64 decorate DivisionCard title
-# ```
-
-# :white_check_mark: Result: 以下のように表示されればOKです
-
-# ![2022-05-08_15h01_24.png](https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/75738/cd36d93a-c0bb-39eb-5932-54a85bfe2401.png)
+# ![2022-05-08_15h01_24.png](https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/75738/1f6fb19c-95a6-95e1-fc3d-33a602a30260.png)
 
 # 各部門に属する部署の数と人数を表示してみましょう。GraphQLにフィールド `numDepartments` と `numMembers` を追加します。
 
 # :large_orange_diamond: Action: 以下のコマンドを入力してください。
 
 # ```terminal
-git apply
-be7ec9c add numMembers and numDepartments
+git apply patches/a806910.patch
 # ```
 
 # :white_check_mark: Result: 以下のように表示されればOKです
 
-# ![2022-05-08_15h00_54.png](https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/75738/c05f3746-9b25-2053-6a05-dc04f3220e3d.png)
+# ![2022-05-08_15h00_54.png](https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/75738/1be85e13-4b3c-684f-56f5-1c903ed59ee3.png)
 
 # 各部門のカード右下に英語部門名も表示してみましょう。GraphQLにフィールド `divisionDisplayNameEn` を追加します。
 
 # :large_orange_diamond: Action: 以下のコマンドを入力してください。
 
 # ```terminal
-git apply
-00f6142 add English division names
+git apply patches/737806f.patch
 # ```
 
 # :white_check_mark: Result: 以下のように表示されればOKです
 
-# ![2022-05-08_15h19_35.png](https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/75738/925aeeee-0ee8-6b6d-9d3c-b233e92dd808.png)
+# ![2022-05-08_15h19_35.png](https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/75738/e74238b5-8f19-8be3-2ec7-df9608258e49.png)
 
-# 表示される部門の数を増やしてみましょう。そのためには、GraphQL Queryの`divisions`フィールドの戻り値の要素数を増やします。
+# 最後に、表示される部門の数を増やしてみましょう。そのためには、GraphQL Queryの`divisions`フィールドの戻り値の要素数を増やします。
 
 # :large_orange_diamond: Action: 以下のコマンドを入力してください。
 
 # ```terminal
-git apply
-78c545c (client-after-server) more divisions
+git apply patches/3f1bc4f.patch
 # ```
 
 # :white_check_mark: Result: 以下のように表示されればOKです
